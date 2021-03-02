fdc7912548c4cf5008814d7dfde793f6f54d23f6
Vikas Kumar <mr.rajput7547@gmail.com>
2021-03-02 19:35:47 +0530

    Update service.dart

--- lib/service.dart
+++ lib/service.dart
@@ -6,7 +6,7 @@ import './model.dart';
 
 Future<WeatherModel> getWeather(String latitude,String longitude) async {
   final response = await http.get(
-      'https://api.openweathermap.org/data/2.5/onecall?lat=$latitude&lon=$longitude&exclude={part}&appid=8816edade9e940ce357d6c8633cb2352');
+      'https://api.openweathermap.org/data/2.5/onecall?lat=$latitude&lon=$longitude&exclude={part}&appid=8816edade9e940ce357d6c8633c****');
   if (response.statusCode == 200) {
     final data = WeatherModel.fromJson(jsonDecode(response.body));
     return data;
